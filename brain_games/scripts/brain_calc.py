38d37b372f9113521a52a740c0e619ba90078e74
Alexandr Knyazev <a.knyazev944@gmail.com>
2024-09-05 14:24:25 +0300

    brain_calc.py

---
+++ brain_games/scripts/brain_calc.py
@@ -0,0 +1,32 @@
+import random
+import math
+import operator
+
+def calc():
+    for i in range(3):
+        numb_1 = random.randint(1, 5)
+        numb_2 = random.randint(1, 5)
+        operation = random.choice(['+', '-', '*'])
+        examples = f"{numb_1} {operation} {numb_2}"
+        print('Question: ' + examples)
+        player_answer = int(input('Your answer: '))
+        correct_answer = None
+        if operation == '+':
+            correct_answer = sum([numb_1, numb_2])
+        elif operation == '-':
+            correct_answer = operator.sub(numb_1, numb_2)
+        elif operation == '*':
+            correct_answer = math.prod([numb_1, numb_2])
+        if player_answer == correct_answer:
+            print('Correct!')
+        # elif player_answer == str(player_answer):
+        #     print('Error!')
+        #     return
+        elif player_answer != correct_answer:
+            print(f"'{player_answer}' is a wrong answer ;(. The correct answer was '{correct_answer}'. \nLet's try again, NAME!")
+            return
+    print('Congratulations, NAME!')
+
+
+
+calc()
