21fc5f4fce5097dc5322b96f6da5770599e6cee1
Alexandr Knyazev <a.knyazev944@gmail.com>
2024-09-05 15:01:57 +0300

    brain_gcd.py

---
+++ brain_games/scripts/brain_gcd.py
@@ -0,0 +1,20 @@
+import math
+import random
+
+
+print('Find the greatest common divisor of given numbers.')
+def gcd():
+    for i in range(3):
+        numb_1 = random.randint(1, 5)
+        numb_2 = random.randint(1, 5)
+        examples = f'{numb_1} {numb_2}'
+        print('Question: ' + examples)
+        player_answer = int(input('Your answer: '))
+        if player_answer == math.gcd(numb_1, numb_2):
+            print('Correct!')
+        elif player_answer != math.gcd(numb_1, numb_2):
+            print(f"'{player_answer}' is a wrong answer ;(. Correct answer was '{math.gcd(numb_1, numb_2)}'\nLet's try again, NAME!")
+            return
+    print('Congratulations!')
+
+gcd()
