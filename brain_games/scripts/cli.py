c1c32960dd1dd4799e88d57d4b217f1a59a2b56f
Alexandr Knyazev <a.knyazev944@gmail.com>
2024-09-11 14:21:10 +0300

    модуль brain_games -> _games

--- brain_games/scripts/cli.py
+++ brain_games/scripts/cli.py
@@ -2,8 +2,10 @@ import prompt
 
 
 def welcome_user():
-    name = prompt.string('May I have yor name? ')
+    print('May I have your name? ', end='')
+    name = input()
     print(f"Hello, {name}"'!')
     return
+
 
 
