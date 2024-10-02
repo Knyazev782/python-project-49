08562b5e88fd8fdc2c64ba586111b53298edc549
Alexandr Knyazev <a.knyazev944@gmail.com>
2024-10-02 17:31:33 +0300

    последние 2 ошибочки

--- brain_games/games/brain_even.py
+++ brain_games/games/brain_even.py
@@ -7,7 +7,7 @@ def get_game_data():
     random_number = random.randint(1, 100)
     question = random_number
     if question % 2 == 0:
-     answer = 'yes'
+        answer = 'yes'
     else:
-     answer = 'no'
+        answer = 'no'
     return question, answer
